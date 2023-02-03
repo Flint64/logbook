2298092b3f2da49bff17406ee005b55b9b651e0f
flint64 <twda96@gmail.com>
2023-02-02 20:38:39 -0700

    and more comments in combatservice

--- src/app/services/combat.service.ts
+++ src/app/services/combat.service.ts
@@ -26,42 +26,6 @@ export class CombatService {
   enemyATBValues: number[] = [];
   
   constructor() { }
-  
-//   attackFunc = function(enemy = null){
-//     console.log(enemy);
-//   }
-
-//   magicFunc = function(){
-//     console.log('magic menu');
-//   }
-
-//   inventoryFunc = function(){
-//     console.log('inventory menu');
-//   }
-
-//   testFunc = function(word = null){
-//     console.log('Test Func ' + word)
-//   }
-
-//  attack = {
-//   name: 'Attack',
-//   func: this.attackFunc
-//   }
-
-//   magic = {
-//     name: 'Magic',
-//     func: this.magicFunc
-//   }
-
-//   inventory = {
-//     name: 'Inventory',
-//     func: this.inventoryFunc
-//   }
-
-//   test = {
-//     name: 'Test',
-//     func: this.testFunc
-//   }
 
   //Only put 8 elements in the array as the last 9th will either be pause or a back button
   // mainMenuOptions = [this.attack, this.magic, this.inventory];
