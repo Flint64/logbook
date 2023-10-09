e7f03d586bc068d1cdd46c4a8f33425db48cc31b
flint64 <twda96@gmail.com>
2023-10-09 13:50:16 -0600

    removed comment

--- src/app/models/effect.model.ts
+++ src/app/models/effect.model.ts
@@ -15,5 +15,3 @@ export class Effect {
     self: boolean
     helpDescription: string
 }
-
-//TODO: Lucky hit type for some effects. Right now everything is 100% chance to be applied barring any resistances. Add a way for effects to have like a 20% proc chance, then check for resistances to see if it gets applied or not 
